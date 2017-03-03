ee749e16b3b418bf72a9145cc43c0456d95c0206
Assad Zakir <assadzakirhussain@gmail.com>
2017-03-03 20:42:56 +0500

    update home images

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,7 +8,7 @@ import FlatButton from 'material-ui/FlatButton';
 const Home = () => (
     <Card>
         <CardMedia>
-            <img src="http://img01.deviantart.net/9670/i/2010/349/9/2/cybercrime_by_unite7-d34xd80.jpg" />
+            <img src="http://walkonomics.com/blog/wp-content/uploads/2012/02/Nicobobinus_Tottenham_Riot.jpg" />
         </CardMedia>
     </Card>
 );
